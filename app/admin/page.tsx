885f164cd8ed9d63992ee8cfc676af20588755a0
jairov96 <jairo.martin.cabello@gmail.com>
2023-09-27 11:37:26 +0200

    remove login buttons from admin main page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,6 @@
 "use client";
-import AuthProvider from "@/middleware/authProvider";
-import { useSession } from "next-auth/react";
-import { signIn, signOut } from "next-auth/react";
 
 const AdminPage: React.FC = () => {
-  const { data: session, status } = useSession();
 
   const handleEdit = (postId: string) => {
     window.location.href = `/admin/edit/${postId}`;
@@ -16,20 +12,9 @@ const AdminPage: React.FC = () => {
   };
 
   return (
-    <AuthProvider>
-      <div className="container mx-auto p-6">
-        <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
-
-        {status === 'authenticated' ?
-          <button className="bg-sky-600" onClick={() => signOut()}>SignOut</button>
-          :
-          <button className="bg-sky-300" onClick={() => signIn("credentials")}>Sign in</button>
-        }
-
-        
-
-      </div>
-    </AuthProvider>
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
+    </div>
   );
 };
 
